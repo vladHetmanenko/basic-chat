7cdf6a99b34a2c9cfca64c6bb97e16e0a5a07358
Vlad Hetmanenko <hetmanenkovlad710@gmail.com>
2025-03-28 10:25:24 +0200

    fix(): update

--- server_a/messenger/constants.py
+++ server_a/messenger/constants.py
@@ -1,3 +1,3 @@
 WAIT_MESSAGE = "Wait ..."
 
-NGROK_SERVER2_URL = "https://3c30-160-238-124-33.ngrok-free.app/update_message/" # Changee
+NGROK_SERVER2_URL = "https://5ddc-2a02-2378-1025-1801-35b0-b72-74be-2736.ngrok-free.app/update_message/" # Changee
